560d821869a8c6b108708801e5f3733ef291fb10
Vivin Wilson <vivinwilson@hungerkind.local>
2019-04-10 21:43:07 -0400

    adding support to broadcast multiple files; adding mapper and reducer classes to cluster jobs

--- src/main/java/kmeans/KMeans.java
+++ src/main/java/kmeans/KMeans.java
@@ -5,7 +5,11 @@ import java.io.IOException;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocatedFileStatus;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.RemoteIterator;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
@@ -33,30 +37,45 @@ public class KMeans extends Configured implements Tool {
         // FileInputFormat.addInputPath(job, new Path(args[0]));
         // FileOutputFormat.setOutputPath(job, new Path(args[1]));
         // return job.waitForCompletion(true)? 0 : 1;
+
         int counter = 0;
+        int ret;
         while(true){
             Configuration conf = getConf();
+            conf.set("mapreduce.output.textoutputformat.separator", ":");
             conf.set("K", args[2]);
             Job job = Job.getInstance(conf,"clustering");
             job.setJarByClass(KMeans.class); 
-            job.setMapperClass(KMeansMapper.class);   
-            job.setOutputKeyClass(Text.class);
+            job.setMapperClass(KMeansMapper.class); 
+            job.setReducerClass(KMeansReducer.class);  
+            job.setOutputKeyClass(IntWritable.class);
             job.setOutputValueClass(Text.class);
-            job.addCacheFile((new Path("centroids" + counter)).toUri());
-            counter++;
             FileInputFormat.addInputPath(job, new Path(args[0]));
-            FileOutputFormat.setOutputPath(job, new Path("centroids" + counter));
+
             // broadcast centroids for next iteration 
-            //termination condition ??
-            return job.waitForCompletion(true)? 0 : 1;
+            FileSystem fs = FileSystem.get(conf);
+            RemoteIterator<LocatedFileStatus> files = fs.listFiles(new Path("centroids" + counter), false);
+            while(files.hasNext()){
+                Path filePath = files.next().getPath();
+                logger.info("ADDING FILES TO CACHE : "+ filePath.toString());
+                job.addCacheFile(filePath.toUri());
+            }
+            
+            counter++;
+            FileOutputFormat.setOutputPath(job, new Path("centroids" + counter));
+            ret =  job.waitForCompletion(true)? 0 : 1;
+
+            //TODO termination condition 
+            if(counter == 2){
+                break;
+            }
         }
-
-
+        return ret;
     }
 
     public static void main(final String[] args) {
-        if (args.length != 2) {
-            throw new Error("Two arguments required:\n<input-dir> <output-dir>");
+        if (args.length != 3) {
+            throw new Error("Two arguments required:\n<input-dir> <output-dir> <K>");
         }
 
         try {
