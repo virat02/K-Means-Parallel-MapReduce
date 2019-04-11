46dec821f709acb490c42d12a2cbef7917ea9198
Vivin Wilson <vivinwilson@hungerkind.local>
2019-04-10 21:43:51 -0400

    adding logic for mapper and reducer class

--- src/main/java/kmeans/KMeansMapper.java
+++ src/main/java/kmeans/KMeansMapper.java
@@ -1,4 +1,4 @@
-package  kmeans;
+package kmeans;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -9,36 +9,78 @@ import java.util.List;
 
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class KMeansMapper extends Mapper<Object, Text, Text, Text> {
+public class KMeansMapper extends Mapper<Object, Text, IntWritable, Text> {
 
-    List<Double> centroids = new ArrayList<>();
+    List<ArrayList<Double>> centroids = new ArrayList<>();
+    int K = 0;
 
     @Override
-    protected void setup(Mapper<Object, Text, Text, Text>.Context context) throws IOException, InterruptedException {
+    protected void setup(Mapper<Object, Text, IntWritable, Text>.Context context)
+            throws IOException, InterruptedException {
         super.setup(context);
 
         URI[] files = context.getCacheFiles();
+        K = context.getConfiguration().getInt("K", 0);
 
-        for (URI f : files){
+        for (int i = 0; i < K; i++) {
+            centroids.add(new ArrayList<Double>());
+        }
+
+        for (URI f : files) {
             FileSystem fs = FileSystem.get(f, context.getConfiguration());
 
             Path path = new Path(f.toString());
             BufferedReader br = new BufferedReader(new InputStreamReader((fs.open(path))));
 
             String line;
-
-            while ((line = br.readLine()) != null){
-                centroids.add(Double.parseDouble(line));
+            while ((line = br.readLine()) != null) {
+                String[] keyvalue = line.split(":");
+                int index = Integer.parseInt(keyvalue[0]);
+                String[] temp = keyvalue[1].split(";");
+                ArrayList<Double> record = centroids.get(index);
+                for (String t : temp) {
+                    record.add(Double.parseDouble(t));
+                }
             }
             br.close();
         }
     }
 
-    @Override
-    public void map(final Object key, final Text value, final Context context) throws IOException, InterruptedException {
+    private Double distance(ArrayList<Double> centroid, ArrayList<Double> record) {
+        double sum = 0.0;
+        int size = centroid.size();
+        // ignoring the last elememt ... which is the actual label for now 
+        for (int i = 0; i < size - 1; i++) {
+            sum += Math.pow(centroid.get(i) - record.get(i), 2);
+        }
+        return Math.sqrt(sum);
+    }
 
+    @Override
+    public void map(final Object key, final Text value, final Context context)
+            throws IOException, InterruptedException {
+        Double min = null;
+        Integer minCentroidKey = null;
+        int size = centroids.size();
+        double dist = 0;
+        ArrayList<Double> record = new ArrayList<Double>();
+        String[] temp = value.toString().split(";");
+        for (String t : temp) {
+            record.add(Double.parseDouble(t));
+        }
+
+        for (int i = 0; i < size; i++) {
+            dist = distance(centroids.get(i), record);
+            if (min == null || min > dist) {
+                min = dist;
+                minCentroidKey = i;
+            }
+        }
+
+        context.write(new IntWritable(minCentroidKey), value);
     }
 }
--- src/main/java/kmeans/KMeansReducer.java
+++ src/main/java/kmeans/KMeansReducer.java
@@ -1,14 +1,43 @@
 package kmeans;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public  class KMeansReducer extends Reducer<Text, Text, Text, Text> {
-   @Override
-   protected void reduce(Text arg0, Iterable<Text> arg1, Reducer<Text, Text, Text, Text>.Context arg2)
-           throws IOException, InterruptedException {
-       super.reduce(arg0, arg1, arg2);
-   }
+public class KMeansReducer extends Reducer<IntWritable, Text, IntWritable, Text> {
+    @Override
+    protected void reduce(IntWritable arg0, Iterable<Text> arg1,
+            Reducer<IntWritable, Text, IntWritable, Text>.Context arg2) throws IOException, InterruptedException {
+        ArrayList<Double> centroid = new ArrayList<Double>();
+        ArrayList<ArrayList<Double>> records = new ArrayList<>();
+        
+        for (Text t : arg1) {
+            ArrayList<Double> record = new ArrayList<Double>();
+            for (String i : t.toString().split(";")) {
+                record.add(Double.parseDouble(i));
+            }
+            records.add(record);
+        }
+
+        int size = records.get(0).size();
+        for (int i = 0; i < size; i++) {
+            centroid.add(0.0);
+        }
+
+        for (ArrayList<Double> r : records) {
+            for (int i = 0; i < size; i++) {
+                centroid.set(i, centroid.get(i) + r.get(i));
+            }
+        }
+
+        String[] value = new String[size];
+        for (int i = 0; i < size; i++) {
+            value[i] = Double.toString(centroid.get(i) / records.size());
+        }
+
+        arg2.write(arg0, new Text(String.join(";", value)));
+    }
 }
