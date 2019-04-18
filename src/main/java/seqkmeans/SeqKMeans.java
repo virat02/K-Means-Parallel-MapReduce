1032e8c1c84c8c07b3a62f4f05789868cce0e0fe
Javesh Monga <javeshmonga@gmail.com>
2019-04-17 23:29:56 -0400

    refactord

--- src/main/java/seqkmeans/SeqKMeans.java
+++ src/main/java/seqkmeans/SeqKMeans.java
@@ -11,23 +11,23 @@ public class SeqKMeans {
 
     private static ArrayList<ArrayList<Double>> records = new ArrayList<>();
     private static ArrayList<ArrayList<Double>> centroids = new ArrayList<>();
-    private static int k = 2;
-    private static HashMap<ArrayList<Double>, Integer> result = new HashMap<>();
+    private static int k = 10;
+    private static HashMap<ArrayList<Double>, Double> centroidMap = new HashMap<>();
 
     // Manhattan distance
     private static Double distance(ArrayList<Double> centroid, ArrayList<Double> record)
     {
         double sum = 0.0;
         int size = centroid.size();
-        // ignoring the last element ... which is the actual label for now
-        for (int i = 0; i < size - 1; i++) {
+        // ignoring the second last element ... which is the actual label for now
+        // and the last element which is the centroid to which the record belongs
+        for (int i = 0; i < size - 2; i++) {
             sum += Math.abs(centroid.get(i) - record.get(i));
         }
         return sum;
     }
 
-    public static void main(String[] args) throws IOException {
-
+    private static void readFile() throws IOException {
         // Building our dataset in memory
         BufferedReader br = new BufferedReader(new FileReader("input/winequality-red.csv"));
         String line;
@@ -37,10 +37,13 @@ public class SeqKMeans {
             for (String s : recordValuesAsString) {
                 record.add(Double.parseDouble(s));
             }
+            record.add(0.0);
             records.add(record);
         }
         br.close();
+    }
 
+    private static void selectRandomCentroids() {
         // Selecting k centroids at random
         // Reference - https://stackoverflow.com/questions/12487592/randomly-select-an-item-from-a-list
         int i = 0;
@@ -50,23 +53,51 @@ public class SeqKMeans {
             centroids.add(centroid);
             i++;
         }
+    }
 
+    private static void fillCentroidMap() {
         // creating k entries in hashmap, one for each centroid
-        i = 1;
+        Double j = 1.0;
         for (ArrayList<Double> c: centroids) {
-            result.put(c, i);
-            i++;
+            centroidMap.put(c, j);
+            j++;
         }
 
-        System.out.println(result);
+        // To check how many clusters were there after random picking
+//        for (ArrayList<Double> a: centroidMap.keySet()) {
+//            System.out.println(centroidMap.get(a));
+//        }
+    }
+
+    private static void kMeans(){
 
         // running k means
-        for (ArrayList<Double> r: records) {
-            Double min_val = Double.MAX_VALUE;
-            ArrayList<Double> selected_centroid;
-            for (ArrayList<Double> c: centroids) {
-                Double d = distance(c, r);
+        int size = records.get(0).size();
+        boolean flag = false;
+        do{
+            for (ArrayList<Double> r: records) {
+                Double min_val = Double.MAX_VALUE;
+                for (ArrayList<Double> c: centroids) {
+                    Double d = distance(c, r);
+                    if (d < min_val){
+                        r.set(size-1, centroidMap.get(c));
+                        min_val =d;
+                        flag = true;
+                    }
+                }
             }
+        } while(flag);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        readFile();
+        selectRandomCentroids();
+        fillCentroidMap();
+        kMeans();
+
+        for (ArrayList<Double> r: records) {
+            System.out.println(r);
         }
     }
 }
